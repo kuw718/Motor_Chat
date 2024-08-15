047645c26c8cf7398ca0d763e1d6ea0d98ef319f
kuw718 <showya986@gmail.com>
2024-08-15 09:17:56 +0000

    氏名の文字数制限を追加、それに伴いフォロー・フォロー解除ボタンの大きさを変更

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,8 @@
 class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  validates :name, presence: true, uniqueness: true, length: { maximum: 13 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
